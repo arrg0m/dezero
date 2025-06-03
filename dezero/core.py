34116d1906ea0661ba96085cef6ca5e70e865543
arrg0m <hyeongmin.byun@gmail.com>
2025-06-03 14:59:21 +0900

    step 38: reshape and transpose

--- dezero/core.py
+++ dezero/core.py
@@ -3,6 +3,8 @@ import contextlib
 import weakref
 import numpy as np
 
+import dezero
+
 
 def as_array(x):
     if np.isscalar(x):
@@ -109,6 +111,18 @@ class Variable:
             return 'variable(None)'
         p = str(self.data).replace('\n', '\n' + ' ' * 9)
         return 'variable(' + p + ')'
+    
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+    
+    def transpose(self):
+        return dezero.functions.transpose(self)
+    
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
 
 class Function:
     def __call__(self, *inputs):
