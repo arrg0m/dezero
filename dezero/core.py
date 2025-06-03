07824552423d6ee87d38f7485740e645f1ecb9b8
arrg0m <hyeongmin.byun@gmail.com>
2025-06-03 15:01:39 +0900

    step 38 additional impl

--- dezero/core.py
+++ dezero/core.py
@@ -117,12 +117,17 @@ class Variable:
             shape = shape[0]
         return dezero.functions.reshape(self, shape)
     
-    def transpose(self):
-        return dezero.functions.transpose(self)
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1:
+            if isinstance(axes[0], (tuple, list)) or axes[0] is None:
+                axes = axes[0]
+        return dezero.functions.transpose(self, axes)
     
     @property
     def T(self):
-        return dezero.functions.transpose(self)
+        return dezero.functions.transpose(self, axes)
 
 class Function:
     def __call__(self, *inputs):
